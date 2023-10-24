9bfec838d1aba877988821db0579489e99327f2a
ramesh <kaviyamudan.21@ichat.sp.edu.sg>
2023-10-24 16:24:49 +0800

    temp calculation

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,19 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("display_main_menu")
+def calc_average(list):
+    avg=sum(list)/len(list)
+    print("Average temperature is:" + str(avg))
+def calc_min_max(list):
+    print("The minimum value is ")
+    print(min(list))
+    print("The maximum value is ")
+    print(max(list))
+print("Enter temp values separated by commas and press 'Enter'  e.g: 5,8,9,3")
+tempstring=input()
+tempstringList=tempstring.split(",")
+print(tempstringList)
+tempfloatlist=list(map(float,tempstringList))
+print(str(tempfloatlist))
+calc_average(tempfloatlist)
+calc_min_max(tempfloatlist)
